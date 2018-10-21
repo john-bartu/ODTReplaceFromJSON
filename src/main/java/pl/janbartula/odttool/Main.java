f02bae1c3f7804a21399307ae87550fdcfdab3a2
john_bartu <bartula.jan@gmail.com>
2018-10-22 01:16:23 +0200

    Refactor, JavaDocs, Optimisation

---
+++ src/main/java/pl/janbartula/odttool/Main.java
@@ -0,0 +1,156 @@
+package pl.janbartula.odttool;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main
+{
+    public static String tempDirectory = "temp/";
+    private static String inputDirectory = "input/";
+    private static String outputDirectory = "output/";
+
+
+    private static String templateFile = "content.xml";
+    private static String dataFile = "data.json";
+
+    public static void main(String[] args)
+    {
+
+        try
+        {
+            //Check if input data directory exist
+            if (Files.notExists(Paths.get(inputDirectory)))
+            {
+                System.out.println("Error: Input directory does not exist.");
+                return;
+            }
+
+            //Check if input JSON data exist
+            if (Files.notExists(Paths.get(inputDirectory + dataFile)))
+            {
+                System.out.println("Error: JSON file does not exist in input directory.");
+                return;
+            }
+
+
+            //Check if temporary directory exist
+            if (Files.notExists(Paths.get(tempDirectory)))
+            {
+
+                //If not Create Temporary Directory
+                if (!new File(tempDirectory).mkdirs())
+                {
+                    System.out.println("Error: Making of temporary directory.");
+                }
+            }
+
+            //Check if Output directory exist
+            if (Files.notExists(Paths.get(outputDirectory)))
+            {
+
+                //If not -> Create Output Directory
+                if (!new File(outputDirectory).mkdirs())
+                {
+                    System.out.println("Error: Making of Output directory.");
+                }
+            }
+
+            //Unzip ODT
+            Zipper.unzip(inputDirectory, "template.odt", tempDirectory);
+
+
+            //Check if input content data exist
+            if (Files.notExists(Paths.get(inputDirectory + dataFile)))
+            {
+                System.out.println("Error: Cannot find " + dataFile + " in decompressed .odt file.");
+                return;
+            }
+
+
+            //Read ODT Content file
+            String template = readFile(tempDirectory + templateFile);
+
+
+            //Read JSON Data
+            String jsonData = readFile(inputDirectory + dataFile);
+            JSONObject obj = new JSONObject(jsonData);
+            JSONArray arr = obj.getJSONArray("datas");
+
+
+            File srcDir = new File(tempDirectory);
+
+            //for each objects in array replace gap with data from JSON
+            for (int index = 1; index < arr.length(); index++)
+            {
+
+                //Read Json Data at Index in Array
+                String name = arr.getJSONObject(index).getString("name");
+                String login = arr.getJSONObject(index).getString("login");
+                String password = arr.getJSONObject(index).getString("password");
+
+                //Copy template and replace gaps
+                String tempTemplate = template;
+                tempTemplate = tempTemplate.replace("{aaaa}", name);
+                tempTemplate = tempTemplate.replace("{bbbb}", login);
+                tempTemplate = tempTemplate.replace("{cccc}", password);
+
+
+                //Save replaced content file to temporary directory
+                saveFile(tempDirectory + templateFile, tempTemplate, StandardCharsets.UTF_8);
+
+                //Zip temporary directory with replaced content file
+                Zipper.zip(tempDirectory, outputDirectory, "File" + index + ".odt");
+            }
+
+            //Remove temporary directory
+            FileUtils.deleteDirectory(srcDir);
+
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * @param path String path to file
+     * @return Read file as String
+     * @throws IOException Exception
+     */
+    static String readFile(String path) throws IOException
+    {
+        return readFile(path, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * @param path     String path to file
+     * @param encoding Encoding od file
+     * @return Read file as String
+     * @throws IOException Exception
+     */
+    static String readFile(String path, Charset encoding) throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+
+    static void saveFile(String path, String content, Charset encoding) throws IOException
+    {
+        try (Writer bufferedWriter = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(path), encoding)
+        ))
+        {
+            bufferedWriter.write(content);
+        }
+
+    }
+}
--- src/main/java/pl/janbartula/odttool/Zipper.java
+++ src/main/java/pl/janbartula/odttool/Zipper.java
@@ -1,4 +1,4 @@
-package zipper;
+package pl.janbartula.odttool;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,52 +13,46 @@ import java.util.zip.ZipOutputStream;
 public class Zipper
 {
 
-        public static void unzip(String file_path, String file_name, String unzip_path) throws IOException
+    public static void unzip(String file_path, String file_name, String unzip_path) throws IOException
     {
-        if(!unzip_path.endsWith("/")){
-            unzip_path+="/";
+        if (!unzip_path.endsWith("/"))
+        {
+            unzip_path += "/";
         }
 
 
-
         final byte[] buffer = new byte[1024];
-        final ZipInputStream zis = new ZipInputStream(new FileInputStream(file_path+file_name));
+        final ZipInputStream zis = new ZipInputStream(new FileInputStream(file_path + file_name));
         ZipEntry zipEntry = zis.getNextEntry();
 
 
-        while (zipEntry != null) {
+        while (zipEntry != null)
+        {
             final String fileName = zipEntry.getName();
-            System.out.println(fileName);
-
-            String filepaths[]= fileName.split("/");
+            String filepaths[] = fileName.split("/");
 
             String path = unzip_path;
-            for(int index = 0; index<filepaths.length-1;index++){
+            for (int index = 0; index < filepaths.length - 1; index++)
+            {
 
 
-
-                path+="/"+filepaths[index];
+                path += "/" + filepaths[index];
                 if (Files.notExists(Paths.get(path)))
                 {
 
                     System.out.println(path);
-                    if(!new File(path).mkdirs()){
+                    if (!new File(path).mkdirs())
+                    {
                         System.out.println("Error Making of temporary directory.");
                     }
                 }
             }
 
-
-
-
-
-
-
-            final File newFile = new File("temp/"+fileName);
-            System.out.println( "File save directory: temp/"+fileName );
+            final File newFile = new File("temp/" + fileName);
             final FileOutputStream fos = new FileOutputStream(newFile);
             int len;
-            while ((len = zis.read(buffer)) > 0) {
+            while ((len = zis.read(buffer)) > 0)
+            {
                 fos.write(buffer, 0, len);
             }
             fos.close();
@@ -69,14 +63,12 @@ public class Zipper
     }
 
 
+    public static void zip(String files_path, String zip_path, String zip_name) throws IOException
+    {
 
-    public static void zip(String files_path, String zip_path, String zip_name ) throws IOException{
-
-        FileOutputStream fos = new FileOutputStream(zip_path+zip_name);
+        FileOutputStream fos = new FileOutputStream(zip_path + zip_name);
         ZipOutputStream zipOut = new ZipOutputStream(fos);
         File fileToZip = new File(files_path);
-
-
 
 
         zipPath(fileToZip, fileToZip.getName(), zipOut);
@@ -87,28 +79,34 @@ public class Zipper
 
     /**
      * @param fileToZip Path to file/directory to zip
-     * @param fileName Path to zipped file.
+     * @param fileName  Path to zipped file.
      * @param zipOut
      * @throws IOException
      */
-    private static void zipPath(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+    private static void zipPath(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException
+    {
 
-        fileName = fileName.replace("tempcopy/","");
-        if (fileToZip.isHidden()) {
+        fileName = fileName.replace(Main.tempDirectory, "");
+        if (fileToZip.isHidden())
+        {
             return;
         }
-        if (fileToZip.isDirectory()) {
-            if (fileName.endsWith("/")) {
+        if (fileToZip.isDirectory())
+        {
+            if (fileName.endsWith("/"))
+            {
                 zipOut.putNextEntry(new ZipEntry(fileName));
                 zipOut.closeEntry();
-            } else {
+            } else
+            {
                 zipOut.putNextEntry(new ZipEntry(fileName + "/"));
                 zipOut.closeEntry();
             }
 
 
             File[] children = fileToZip.listFiles();
-            for (File childFile : children) {
+            for (File childFile : children)
+            {
 
 
                 zipPath(childFile, fileName + "/" + childFile.getName(), zipOut);
@@ -120,7 +118,8 @@ public class Zipper
         zipOut.putNextEntry(zipEntry);
         byte[] bytes = new byte[1024];
         int length;
-        while ((length = fis.read(bytes)) >= 0) {
+        while ((length = fis.read(bytes)) >= 0)
+        {
             zipOut.write(bytes, 0, length);
         }
         fis.close();
